891394d36a3edf569a76732b656461572552e4b8
Ruben Ordonez <ruben@ordonez.us>
2021-03-25 10:40:31 -0700

    Initial Commit
    First commit

---
+++ background.js
@@ -0,0 +1,32 @@
+/*
+Called by browserAction.onClicked
+*/
+function openGoogleVoice() {
+  console.log("openGoogleVoice function called");
+  browser.tabs.create({url: "https://accounts.google.com/ServiceLogin?service=cl&passive=1209600&osid=1&continue=https://voice.google.com&followup=https://voice.google.com&scc=1&authuser=0"});
+}
+browser.browserAction.onClicked.addListener(openGoogleVoice);
+
+/*
+Called when the item has been created, or when creation failed due to an error.
+We'll just log success/failure here.
+*/
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
+}
+
+/*
+Create menu items.
+*/
+browser.menus.create({
+  id: "google-voice",
+  title: "Google Voice",
+  type: "normal",
+  contexts: ["all"],
+  onclick: openGoogleVoice
+}, onCreated);
+
---
+++ readme.txt
@@ -0,0 +1,14 @@
+= Open Google Calendar =
+
+This extension uses the tab features of Thunderbird to open Google
+Voice inside a Thunderbird tab.
+    
+= How it Works =
+    
+This extension adds a button to Thunderbirds main tool bar as well as an item to the
+Tools menu which will open up Google Voice in a new tab.
+
+first use you should see the Google Account login page.  When you login you
+will be prompted by a notification bar which can save your username and password
+for the site.  This password can be removed from the Security -> Passwords
+section of your preferences.
